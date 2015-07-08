5639385485217157814280d80309cc030ef4f29c
EricNY <ericabt@gmail.com>
2015-07-08 15:37:43 -0400

    getting mongolab db setup..

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/docketing-system',
+	db: 'mongodb://admin:andrea100!@ds049568.mongolab.com:49568/gnp-docketing' || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/docketing-system',
 	assets: {
 		lib: {
 			css: [
