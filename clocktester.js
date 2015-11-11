4eec7771a25ec5bbe73efd33f590d4a86d3eb87a
EricNY <ericabt@gmail.com>
2015-11-10 22:16:30 -0500

    fixing gmt issue....

--- clocktester.js
+++ clocktester.js
@@ -95,7 +95,7 @@ var clockLogger = function() {
 
 				if ( dueDateMilliseconds && dueDateMilliseconds.getTime() < monthFromToday && dueDateMilliseconds.getTime() >= today.getTime() ) {
 var d = docs[i].dueDate.toString();
-					trademarkFilings.push(  docs[i].owner + ' ' + docs[i].mark + ' ' + docs[i].applicationNumber + ' ' + d.slice(27) );
+					trademarkFilings.push(  docs[i].owner + ' ' + docs[i].mark + ' ' + docs[i].applicationNumber + ' ' + d.slice(20) );
 
 				}
 				// Mon Aug 24 2015
